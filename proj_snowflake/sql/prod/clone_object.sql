fe2d32cd95f63b7ecabeaf8505fcb6fb23fe00df
RSGill24 <rajindergill2024@gmail.com>
2024-09-16 01:43:14 +0530

    Update clone_object.sql

--- proj_snowflake/sql/prod/clone_object.sql
+++ proj_snowflake/sql/prod/clone_object.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE &prod_db_name CLONE &stage_db_name;
+CREATE DATABASE {{prod_db_name}} CLONE {{stage_db_name}};
 
-USE DATABASE &prod_db_name; 
-ALTER SCHEMA &stage_schema_name RENAME TO &prod_schema_name;
+USE DATABASE {{prod_db_name}}; 
+ALTER SCHEMA {{stage_schema_name}} RENAME TO {{prod_schema_name}};
