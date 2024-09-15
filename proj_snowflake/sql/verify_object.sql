19720d13011331ad739be8a849bbfff2bc8e2cfa
RSGill24 <rajindergill2024@gmail.com>
2024-09-16 00:01:02 +0530

    Create verify_object.sql

---
+++ proj_snowflake/sql/verify_object.sql
@@ -0,0 +1,11 @@
+USE DATABASE <+env.name>_<+stage.variables.DB_name>; 
+
+SELECT COUNT(*) AS number_of_schemas FROM information_schema.schemata WHERE schema_name != 'INFORMATION_SCHEMA';
+
+SELECT COUNT(*) AS number_of_tables FROM information_schema.tables WHERE table_schema != 'INFORMATION_SCHEMA';
+
+SELECT COUNT(*) AS number_of_views FROM information_schema.views WHERE table_schema != 'INFORMATION_SCHEMA';
+
+SELECT COUNT(*) AS number_of_stages FROM information_schema.stages WHERE stage_schema != 'INFORMATION_SCHEMA';
+
+SELECT * FROM employees;
