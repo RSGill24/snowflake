fc7ed46446f014696bb2e0e1b221ed66acb016cb
RSGill24 <rajindergill2024@gmail.com>
2024-09-16 01:29:17 +0530

    Update clone_object.sql

--- proj_snowflake/sql/prod/clone_object.sql
+++ proj_snowflake/sql/prod/clone_object.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE <+env.name>_<+stage.variables.DB_name> CLONE <+pipeline.stages.Create_non_prod_env.spec.env.name>_<+pipeline.stages.Create_non_prod_env.variables.DB_name>;
+CREATE DATABASE &prod_db_name CLONE &stage_db_name;
 
-USE DATABASE <+env.name>_<+stage.variables.DB_name>; 
-ALTER SCHEMA <+pipeline.stages.Create_non_prod_env.spec.env.name>_<+pipeline.stages.Create_non_prod_env.variables.Schema_name> RENAME TO <+env.name>_<+stage.variables.Schema_name>;
+USE DATABASE &prod_db_name; 
+ALTER SCHEMA &stage_schema_name RENAME TO &prod_schema_name;
